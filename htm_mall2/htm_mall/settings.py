a935ea1cec32f2bd2a28580aa491d6c46658b9bd
HwangToeMat <46777602+HwangToeMat@users.noreply.github.com>
2020-02-13 14:08:33 +0900

    .
    .

--- htm_mall2/htm_mall/settings.py
+++ htm_mall2/htm_mall/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%+nk7=%+@6r-uudl0#4m#4k$d*mphd153fxt!4qbq5*1a=5l1&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
